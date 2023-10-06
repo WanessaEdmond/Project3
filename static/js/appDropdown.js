03dbb6e3f10bfe9cf8c3314bf7ed17c4d012ab05
WanessaEdmond <wedmond90@gmail.com>
2023-10-05 22:31:21 -0400

    Updated Drop_down

--- static/js/appDropdown.js
+++ static/js/appDropdown.js
@@ -1,28 +1,61 @@
 let url = 'http://127.0.0.1:5000/nba'
+var _data
 d3.json(url).then((data) => {
-    console.log(data)
+    // console.log(data)
+    _data=data
 })
 
 
-// // function to populate the dropdown menu on the webpage using the names object from samples.json
-// function dropdown_menu(){
+// function to populate the dropdown menu on the webpage using the names object from appDropdown.json
+function dropdown_menu(names){
 
-//     d3.json(url).then((data) => {
-//         console.log(data)
-// let names = data.Player[i].Player
-// let dropdown_selector = d3.select("#selDataset")
-// names.forEach((sample) => {
-//     dropdown_selector 
-//         .append("option")
-//         .text(sample)
-//         .property("value", sample);
-// })})};
-// demographic_table(names[0])
+    d3.json(url).then((data) => {
+        // console.log(data)
+let Playername = data.map(x => x.Player);
+        // console.log(names)
+let dropdown_selector = d3.select("#selDataset")
+Playername.forEach((names) => {
+    dropdown_selector 
+        .append("option")
+        .text(names)
+        .property("value", names);
 
-
+        
+ 
+    
+})
+player_table(names)  
+})};
+ 
     
 //     })
 
 
 // }
+// Calling this function to populate the dropdown menu
+dropdown_menu()
 
+//this function will get call whenever we choose a different value in the dropdown menu 
+function optionChanged(names){
+    player_table(names)   
+}
+
+function player_table(names){
+    _data
+    // let y = d3.json(url).then((data)) 
+
+   let filterdata= _data.filter(_data => _data.Player == names)
+console.log(filterdata)
+let nba_dropdown = d3.select("#Player_Name")
+nba_dropdown.html("")
+Object.entries(filterdata).forEach(entry => {
+    const [key, value] = entry;
+    // console.log(key, value);
+    nba_dropdown
+    .append("h5")
+    .text(`${key}: ${value}`)
+
+});
+}
+
+// player_table()
